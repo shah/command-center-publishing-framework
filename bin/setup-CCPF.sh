6e75c4a61a2fff2eb3a94a1c816537eb73b49484
Shahid N. Shah <shahid@shah.org>
2019-02-16 21:24:38 -0500

    Cleanup installation script

--- bin/setup-CCPF.sh
+++ bin/setup-CCPF.sh
@@ -1,11 +1,7 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-# Install CCPF from GitHub and setup local symlinks into /etc, /usr/lib, etc.
-#
-# TODO: make this script smarter by checking if CCPF is already installed and
-#       doing a git pull to upgrade instead.
-# TODO: only create jsonnet symlink if jsonnet is not already present
+# Install CCPF from GitHub and setup local symlinks
 
 export CCPF_SRC_REPO_URL=https://github.com/shah/command-center-publishing-framework
 export CCPF_HOME="${CCPF_HOME:-/opt/command-center-publishing-framework}"
@@ -13,12 +9,17 @@ export CCPF_BIN_LINKS_PATH="${CCPF_HOME:-/usr/bin}"
 
 sudo apt install -qq make git graphviz
 
-sudo mkdir -p $CCPF_HOME
-sudo git clone $CCPF_SRC_REPO_URL $CCPF_HOME
-sudo ln -s $CCPF_HOME/bin/ccpf-init $CCPF_BIN_LINKS_PATH/ccpf-init
-sudo ln -s $CCPF_HOME/bin/ccpf-make $CCPF_BIN_LINKS_PATH/ccpf-make
-
-echo "CCPF installed in $CCPF_HOME"
-echo "ccpf-init, ccpf-make symlinked in $CCPF_BIN_LINKS_PATH"
+if [ -d "$CCPF_HOME" ]; then
+    cd "$CCPF_HOME"
+    sudo git pull
+    echo "CCPF upgraded in $CCPF_HOME"
+else
+    sudo mkdir -p $CCPF_HOME
+    sudo git clone $CCPF_SRC_REPO_URL $CCPF_HOME
+    sudo ln -s $CCPF_HOME/bin/ccpf-init $CCPF_BIN_LINKS_PATH/ccpf-init
+    sudo ln -s $CCPF_HOME/bin/ccpf-make $CCPF_BIN_LINKS_PATH/ccpf-make
+    echo "CCPF installed in $CCPF_HOME"
+    echo "ccpf-init, ccpf-make symlinked in $CCPF_BIN_LINKS_PATH"
+fi
 
 $CCPF_HOME/bin/doctor.sh
