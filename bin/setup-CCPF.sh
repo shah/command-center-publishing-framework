77f27463fe3300157e68fb18161f263392262cba
Shahid N. Shah <shahid@shah.org>
2019-02-16 21:18:18 -0500

    Cleanup installation script

--- bin/setup-CCPF.sh
+++ bin/setup-CCPF.sh
@@ -9,16 +9,16 @@ set -euo pipefail
 
 export CCPF_SRC_REPO_URL=https://github.com/shah/command-center-publishing-framework
 export CCPF_HOME="${CCPF_HOME:-/opt/command-center-publishing-framework}"
+export CCPF_BIN_LINKS_PATH="${CCPF_HOME:-/usr/bin}"
 
 sudo apt install -qq make git graphviz
 
 sudo mkdir -p $CCPF_HOME
 sudo git clone $CCPF_SRC_REPO_URL $CCPF_HOME
-sudo chmod +x $CCPF_HOME/bin/*
-sudo ln -s $CCPF_HOME/bin/ccpf-init /usr/bin/ccpf-init
-sudo ln -s $CCPF_HOME/bin/ccpf-make /usr/bin/ccpf-make
+sudo ln -s $CCPF_HOME/bin/ccpf-init $CCPF_BIN_LINKS_PATH/ccpf-init
+sudo ln -s $CCPF_HOME/bin/ccpf-make $CCPF_BIN_LINKS_PATH/ccpf-make
 
 echo "CCPF installed in $CCPF_HOME"
+echo "ccpf-init, ccpf-make symlinked in $CCPF_BIN_LINKS_PATH"
 
-#cd $CCPF_HOME/lib/$CCPF_COMPONENT_NAME
-#make check-dependencies
+$CCPF_HOME/bin/doctor.sh
