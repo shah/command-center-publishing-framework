d34a223da7a1395bd92b04fc811f3e15dc9b61cf
Shahid N. Shah <shahid@shah.org>
2019-02-17 18:40:24 -0500

    Added NodeJS vendorize placeholder script (not implemented yet)

---
+++ bin/vendorize-nodejs-ecosystem.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+# Expecting environment variables upon entry:
+# * VENDOR_HOME - the path to install vendor'd items into
+
+# TODO use [fnm](https://github.com/Schniz/fnm), Fast and simple Node.js version manager
