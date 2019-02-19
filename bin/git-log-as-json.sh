d94f780b70ada30de107c8f2f35bd65c10b52b01
Shahid N. Shah <shahid@shah.org>
2019-02-18 21:47:26 -0500

    Create gitlog2json convenience script

---
+++ bin/git-log-as-json.sh
@@ -0,0 +1,40 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+CCPF_HOME="${CCPF_HOME:-/opt/command-center-publishing-framework}"
+JSON_FORMAT=$(cat <<-'END_JSON_FORMAT'
+{
+    "commit": "%H",
+    "abbreviated_commit": "%h",
+    "tree": "%T",
+    "abbreviated_tree": "%t",
+    "parent": "%P",
+    "abbreviated_parent": "%p",
+    "refs": "%D",
+    "encoding": "%e",
+    "subject": "%s",
+    "sanitized_subject_line": "%f",
+    "body": "%b",
+    "commit_notes": "%N",
+    "verification_flag": "%G?",
+    "signer": "%GS",
+    "signer_key": "%GK",
+    "author": {
+        "name": "%aN",
+        "email": "%aE",
+        "date": "%aD"
+    },
+    "commiter": {
+        "name": "%cN",
+        "email": "%cE",
+        "date": "%cD"
+    }
+}
+END_JSON_FORMAT
+)
+
+# Generate Git Log for given directory (or current directory if no path passed in).
+# For each log item, convert it to JSON and pass it to jq which will combine the 
+# lines into a single array. The output can be saved into a .json file.
+
+git log --pretty=format:"$JSON_FORMAT" $@ | $CCPF_HOME/bin/jq --slurp .
